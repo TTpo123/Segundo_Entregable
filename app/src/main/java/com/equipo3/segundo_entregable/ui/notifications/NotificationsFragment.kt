8a7c3f80753b73507e5cf8cb82ab6dff5709da5c
TTpo123 <129268785+TTpo123@users.noreply.github.com>
2024-06-20 21:05:54 -0600

    Version 1.4

--- app/src/main/java/com/equipo3/segundo_entregable/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/equipo3/segundo_entregable/ui/notifications/NotificationsFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.equipo3.segundo_entregable.databinding.FragmentNotificationsBinding
 
+
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
